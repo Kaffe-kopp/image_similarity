9716b6f1f005c77f357ec005f8e9e85d3e42709b
hackermanJC <trinity@HackermanJC.>
2025-07-13 23:17:52 +0200

    added writing of average hash to json

---
+++ write_to_json.py
@@ -0,0 +1,33 @@
+
+import os
+import json
+from PIL import Image
+import imagehash
+
+ImgFolderPath = "to_convert"
+
+ImgFolder = os.listdir(ImgFolderPath)
+
+
+with open("hashes.json", "r+") as file:
+    hashes_json = json.load(file)
+    for img in ImgFolder:
+        if img in [i['name'] for i in hashes_json['all_hashes']]: continue
+        name = img
+        series_num = None
+    # add logic here
+        hash_avg = imagehash.average_hash(Image.open(f"{ImgFolderPath}/{img}"))
+
+        img_data = {
+            "name": name,
+            "series_num": series_num,
+            "hash_avg": str(hash_avg)
+            #add more hashes uwu plz
+        }
+
+        hashes_json["all_hashes"].append(img_data)
+
+        file.seek(0)
+
+        json.dump(hashes_json, file, indent=4)
+
